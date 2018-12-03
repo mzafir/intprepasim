8dc14e6cb64ee7fe2f6fd6352777d738e10a38d8
AZ <asim.zafir@gmail.com>
2018-12-03 02:38:57 -0800

    Remove duplicates from LinkedLists. I am getting null point exception - some how reference to head is not passed or is reference as null.

--- src/com/asim/interviewr2/SinglyLinkedListNode.java
+++ src/com/asim/interviewr2/SinglyLinkedListNode.java
@@ -5,8 +5,8 @@ public class SinglyLinkedListNode {
     SinglyLinkedListNode next;
 
     public SinglyLinkedListNode(int data) {
-        data = this.data;
-        next = null;
+        this.data = data;
+        this.next = null;
     }
 
 
@@ -17,33 +17,45 @@ public class SinglyLinkedListNode {
         Ref = head.next;
         int temp = head.data;
 
-        while (Prev.next != null) {
-            temp = Prev.data;
-            Ref = Ref.next;
-            if (Ref.data == temp)
+        while (Prev != null && Prev.next != null) {
+
+            if (head.data == head.next.data) {
+                head.next = head.next.next;
+            }
+            //      temp = Prev.data;
+            //Ref = Ref.next;
+            if (Prev.data == Prev.next.data)
             //found duplicate
             {
-                Prev.next = Ref.next.next;
-                Ref.next = null;
+                Prev.next = Prev.next.next;
+
 
             }
+            //Ref
             Prev = Prev.next;
         }
-        Ref = head;
-        return Ref;
 
+
+        return head;
+
+    }
+
+    public static void printList(SinglyLinkedListNode ptr) {
+        while (ptr.next == null) {
+            System.out.print( ptr.data + "-->" );
+        }
     }
 
 
     public static void main(String[] args) {
 
         System.out.println( "this is a test" );
-        SinglyLinkedListNode n1 = new SinglyLinkedListNode( 3 );
-        SinglyLinkedListNode n2 = new SinglyLinkedListNode( 4 );
-        SinglyLinkedListNode n3 = new SinglyLinkedListNode( 5 );
-        SinglyLinkedListNode n4 = new SinglyLinkedListNode( 6 );
-        SinglyLinkedListNode n5 = new SinglyLinkedListNode( 3 );
-        SinglyLinkedListNode n6 = new SinglyLinkedListNode( 4 );
+        SinglyLinkedListNode n1 = new SinglyLinkedListNode( 1 );
+        SinglyLinkedListNode n2 = new SinglyLinkedListNode( 1 );
+        SinglyLinkedListNode n3 = new SinglyLinkedListNode( 1 );
+        SinglyLinkedListNode n4 = new SinglyLinkedListNode( 2 );
+        SinglyLinkedListNode n5 = new SinglyLinkedListNode( 2 );
+        SinglyLinkedListNode n6 = new SinglyLinkedListNode( 3 );
         SinglyLinkedListNode p4 = null;
 
         n1.next = n2;
@@ -55,6 +67,8 @@ public class SinglyLinkedListNode {
 
         p4 = n1;
 
-        removeDuplicates( p4 );
+
+        printList( removeDuplicates( p4 ) );
+
     }
 }
